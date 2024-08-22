e8334362ae808829c8987986fcf041351ae6c574
incognito-interactive <125911680+incognito-interactive@users.noreply.github.com>
2024-08-22 15:35:08 +0200

    func: add more config options

--- src/mod.ts
+++ src/mod.ts
@@ -3,11 +3,23 @@ import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { DependencyContainer } from "tsyringe";
 
 import {
+    enabled,
+    logBalancingWarnings,
     multiplier
 } from "../config/config.json";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
 
 class Overdamage implements IPostDBLoadMod {
     public postDBLoad(container: DependencyContainer): void {
+        // Return if the mod is disabled.
+        if (!enabled) return;
+
+        // Get the logger.
+        const logger = container.resolve<ILogger>("WinstonLogger");
+
+        // If warning is enabled and the multiplier is set incredibly high or too low then log a warning.
+        if (logBalancingWarnings && multiplier < 1 || multiplier > 10) logger.warning("Overdamage: The recommended multiplier range is between '1.5' to '3.0'!")
+
         // Get the Database Server.
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
 
